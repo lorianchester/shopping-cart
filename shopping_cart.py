0931a27844e5894da88532ec69b28b0307276324
lorianchester <lec128@georgetown.edu>
2022-02-21 02:29:27 -0500

    fixed while loop

--- shopping_cart.py
+++ shopping_cart.py
@@ -54,33 +54,25 @@ if __name__ == "__main__":
 
         #validate user input
         #product IDs are valid between 1 and 20
-        #if valid, add to list of inputs
+        #if valid, add to list
         if product_id > 0 and product_id < 20:
     
-            clerk_inputs[product] = product_id
+            matching_products = []
+            prices = []
+            array_index = 0
 
-            product = product + 1
+            for x in products:
+                if str(x["id"]) == str(clerk_inputs[array_index]):
+                #this is a match
+                matching_products.append(x["name"])
+                prices.append(x["price"])
+                array_index = array_index + 1
         #not valid input
         else:
 
             print("Are you sure that product identifier is correct? Please try again!")
 
         product_id = input("Please input a product identifier: ")
-
-
-
-    # LOOK UP CORRESPONDING PRODUCTS AND PRICES
-
-    matching_products = []
-    prices = []
-    array_index = 0
-
-    for x in products:
-        if str(x["id"]) == str(clerk_inputs[array_index]):
-            #this is a match
-            matching_products.append(x["name"])
-            prices.append(x["price"])
-            array_index = array_index + 1
 
 
     # print receipt
